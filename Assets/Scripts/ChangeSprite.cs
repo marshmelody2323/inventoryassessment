f8174150a6731d9eb100ddefbfb9ec339b3be847
Melody Lynn <mysticcreeper99@gmail.com>
2021-06-24 14:38:45 +1000

    Update 8
    Finished sprite switching

--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -5,38 +5,78 @@ using UnityEngine.UI;
 public class ChangeSprite : MonoBehaviour
 {
     private bool sprite1 = false;
+    private bool sprite2 = false;
     private Image SourceImage;
-    public Sprite sprite2;
+    public Sprite sprite1Sprite;
+    [UnityEngine.Serialization.FormerlySerializedAs("sprite2")]public Sprite sprite2Sprite;
+    public Sprite sprite3Sprite;
+
+    private bool setSprite1 = false;
+    private bool setSprite2 = false;
 
     void Start()
     {
         SourceImage = gameObject.GetComponent<Image>(); //get sprite renderer & store it
     }
 
-    public void Sprite1()
+    public void ChangeToSprite1()
     {
-        if(sprite1 == false)
+        if(sprite1 == false && sprite2 == false)
         {
-            //hasnt been pressed
+            SourceImage.sprite = sprite1Sprite;
+        }
+        else 
+        {
+            SourceImage.sprite = sprite3Sprite;
         }
 
-        
-    }
-
-    public void Sprite2()
-    {
-        if (sprite1 == false)
-        {
-            //hasnt been pressed
-        }
-
-        
-    }
-
-    public void change(Sprite differentSprite)
-    {
         sprite1 = true;
-        //spriteRenderer.sprite = differentSprite; //sets sprite renderers sprite
-        SourceImage.sprite = differentSprite;
     }
+
+    public void ChangeToSprite2()
+    {
+        if (sprite1 == false && sprite2 == false)
+        {
+            SourceImage.sprite = sprite2Sprite;
+        }
+        else
+        {
+            SourceImage.sprite = sprite3Sprite;
+        }
+
+        sprite2 = true;
+    }
+
+    //public void change(Sprite differentSprite)
+    //{
+    //    if (sprite1 == false)
+    //    {
+    //        sprite1 = differentSprite == sprite1Sprite;
+    //    }
+
+    //    if (sprite2 == false)
+    //    {
+    //        sprite2 = differentSprite == sprite2Sprite;
+    //    }
+    //    //spriteRenderer.sprite = differentSprite; //sets sprite renderers sprite
+
+    //    if (sprite1 == true && setSprite1 == false)
+    //    {
+    //        // Sprite 1
+    //        SourceImage.sprite = sprite1Sprite;
+    //        setSprite1 = true;
+    //    }
+    //    else if(sprite2 == true && setSprite2 == false)
+    //    {
+    //        // Sprite 2
+    //        SourceImage.sprite = sprite2Sprite;
+    //        setSprite2 = true;
+    //    }
+    //    else if(sprite1 == true && sprite2 == true)
+    //    {
+    //        // Sprite 3 - Combined
+    //        SourceImage.sprite = sprite3Sprite;
+    //    }
+    //    //SourceImage.sprite = differentSprite;
+    //}
 }
--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -8,11 +8,14 @@ public class CharacterScript : MonoBehaviour
     public float jumpForce = 5f;
     public float sprintSpeed = 10f;
     private float movementSpeed;
+    public UnityEngine.UI.Image shopSprite;
+    private SpriteRenderer mySprite;
 
 
     void Start()
     {
         movementSpeed = moveSpeed;
+        mySprite = gameObject.GetComponent<SpriteRenderer>();
     }
 
 
@@ -21,7 +24,7 @@ public class CharacterScript : MonoBehaviour
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * movementSpeed;
-
+        mySprite.sprite = shopSprite.sprite;
 
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
