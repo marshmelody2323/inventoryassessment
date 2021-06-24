15dc52f804fd4e2362b3c67a03da847ea7915502
Melody Lynn <mysticcreeper99@gmail.com>
2021-06-24 12:57:16 +1000

    Update 7
    Started sprite switching

--- Assets/Scripts/ChangeSprite.cs
+++ Assets/Scripts/ChangeSprite.cs
@@ -4,16 +4,38 @@ using UnityEngine.UI;
 
 public class ChangeSprite : MonoBehaviour
 {
-
+    private bool sprite1 = false;
     private Image SourceImage;
+    public Sprite sprite2;
 
     void Start()
     {
         SourceImage = gameObject.GetComponent<Image>(); //get sprite renderer & store it
     }
 
+    public void Sprite1()
+    {
+        if(sprite1 == false)
+        {
+            //hasnt been pressed
+        }
+
+        
+    }
+
+    public void Sprite2()
+    {
+        if (sprite1 == false)
+        {
+            //hasnt been pressed
+        }
+
+        
+    }
+
     public void change(Sprite differentSprite)
     {
+        sprite1 = true;
         //spriteRenderer.sprite = differentSprite; //sets sprite renderers sprite
         SourceImage.sprite = differentSprite;
     }
